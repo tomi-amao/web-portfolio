b4615588167a902b0328ec108da18f34f4a900d7
Tomi Amao <Tomi.Amao@Tomis-MacBook-Air.local>
2024-01-13 14:49:06 +0000

    added interactivity to all section and links to contact section

--- components/about-me/about-me.jsx
+++ components/about-me/about-me.jsx
@@ -1,16 +1,177 @@
+"use client";
+
 import classes from "./about-me.module.css";
 
-import headshot from "../../assets/headshot.jpg";
+import headshot from "../../public/headshot.jpg";
 
 import Image from "next/image";
+import { useState } from "react";
 
 export default function AboutMe() {
   const desc =
     " Through my passion for gaming, I have learned to be resilient, adaptable, and open-minded, always seeking new ways to improve and grow. Whether it is learning a new skill, taking on a new project, or facing a difficult challenge, I approach every opportunity with an unwavering dedication to growth and improvement. Success is not just about achieving a specific goal, but about the personal growth and development that comes along the way.";
-  const fitness = " Fitness drives my commitment to be a better version of myself everyday"
-  const buoyant = " Fitness drives my commitment to be a better version of myself everyday"
-  const volunteer = " Fitness drives my commitment to be a better version of myself everyday"
-  const learner = " Fitness drives my commitment to be a better version of myself everyday"
+  const fitness =
+    " Fitness drives my commitment to be a better version of myself everyday";
+  const resilient =
+    " I find I am at my best when I am facing my toughest challenges, knowing that I will persevere and overcome the many hurdles and roadblocks I will face ";
+  const volunteer =
+    "Giving to others serves plays a major role in my life. It forms one of my core beliefs, giving my a wider persepctive and purpose in life";
+  const learner =
+    " There isn't a day that goes by that I don't think about how I will improve myself. I am constantly driving forward by being a student to all forms of knowledge.";
+
+  const [allTraits, setAllTraits] = useState({
+    fitness: false,
+    resilient: false,
+    volunteer: false,
+    learner: false,
+  });
+
+  const [dotShow, setDotShow] = useState({
+    fitness_dot: true,
+    resilient_dot: true,
+    volunteer_dot: true,
+    learner_dot: true,
+  });
+
+  console.log(allTraits.fitness);
+
+  const fitnessVisibility = {
+    display: allTraits.fitness ? "block" : "none",
+    // order: -1
+  };
+  const resilientVisibility = {
+    display: allTraits.resilient ? "block" : "none",
+    // order: -1
+  };
+  const volunteerVisibility = {
+    display: allTraits.volunteer ? "block" : "none",
+    // order: -1
+  };
+  const learnerVisibility = {
+    display: allTraits.learner ? "block" : "none",
+    // order: -1
+  };
+  const fitnessDot = {
+    display: dotShow.fitness_dot ? "block" : "none",
+  };
+  const resilientDot = {
+    display: dotShow.resilient_dot ? "block" : "none",
+  };
+
+  const volunteerDot = {
+    display: dotShow.volunteer_dot ? "block" : "none",
+  };
+  const learnerDot = {
+    display: dotShow.learner_dot ? "block" : "none",
+  };
+
+  const handleSelectionDotHover = (status, trait) => {
+    if (trait === "fitness_dot") {
+
+      setDotShow((prevValue) => ({
+        ...prevValue,
+        [trait]: status,
+      }))
+      if (!status) {
+        if (allTraits.fitness) {
+          setDotShow((prevValue) => ({
+            ...prevValue,
+            [trait]: true
+          }))
+        }
+      }
+      
+    } else if (trait === "resilient_dot") {
+      setDotShow((prevValue) => ({
+        ...prevValue,
+        [trait]: status,
+      }))
+      if (!status) {
+        if (allTraits.resilient) {
+          setDotShow((prevValue) => ({
+            ...prevValue,
+            [trait]: true
+          }))
+        }
+      }
+
+      ;
+    } else if (trait === "volunteer_dot") {
+      setDotShow((prevValue) => ({
+        ...prevValue,
+        [trait]: status,
+      }))
+      if (!status) {
+        if (allTraits.volunteer) {
+          setDotShow((prevValue) => ({
+            ...prevValue,
+            [trait]: true
+          }))
+        }
+      }
+
+      ;
+    } else if (trait === "learner_dot") {
+      setDotShow((prevValue) => ({
+        ...prevValue,
+        [trait]: status,
+      }))
+      if (!status) {
+        if (allTraits.learner) {
+          setDotShow((prevValue) => ({
+            ...prevValue,
+            [trait]: true
+          }))
+        }
+      }
+
+      ;
+    }
+  };
+
+
+  const handleShowAllTraits = (trait) => {
+    setAllTraits((prevValue) => ({
+      fitness: false,
+      resilient: false,
+      volunteer: false,
+      learner: false,
+    }));
+
+    if (trait === "fitness") {
+      setDotShow(prevValue => ({
+        resilient_dot: false,
+        volunteer_dot: false,
+        learner_dot: false,
+      }))
+    }
+    if (trait === "resilient") {
+      setDotShow(prevValue => ({
+        fitness_dot: false,
+        volunteer_dot: false,
+        learner_dot: false,
+      }))
+    }
+    if (trait === "volunteer") {
+      setDotShow(prevValue => ({
+        fitness_dot: false,
+        resilient_dot: false,
+        learner_dot: false,
+      }))
+    }
+    if (trait === "learner") {
+      setDotShow(prevValue => ({
+        fitness_dot: false,
+        resilient_dot: false,
+        volunteer_dot: false,
+      }))
+    }
+
+    setAllTraits((prevValue) => ({
+      ...prevValue,
+      [trait]: !prevValue[trait],
+    }));
+  };
   return (
     <>
       <h1 className={classes["page-title"]}> About Me </h1>
@@ -30,47 +191,80 @@ export default function AboutMe() {
           </div>
         </div>
         <div className={classes["trait-large"]}>
-          <li>
+          <li
+            onClick={() => {
+              handleShowAllTraits("fitness");
+            }}
+            onMouseOver={() => {
+              handleSelectionDotHover(true, "fitness_dot");
+            }}
+            onMouseOut={() => {
+              handleSelectionDotHover(false, "fitness_dot");
+            }}
+          >
+            <span style={fitnessDot}></span>
             <p>fitness advocate</p>
           </li>
-          <li>
+          <li
+            onClick={() => {
+              handleShowAllTraits("resilient");
+            }}
+            onMouseOver={() => {
+              handleSelectionDotHover(true, "resilient_dot");
+            }}
+            onMouseOut={() => {
+              handleSelectionDotHover(false, "resilient_dot");
+            }}
+          >
+            <span style={resilientDot}></span>
             <p>buoyant beacon</p>
           </li>
-          <li>
+          <li
+            onClick={() => {
+              handleShowAllTraits("volunteer");
+            }}
+            onMouseOver={() => {
+              handleSelectionDotHover(true, "volunteer_dot");
+            }}
+            onMouseOut={() => {
+              handleSelectionDotHover(false, "volunteer_dot");
+            }}
+          >
+            <span style={volunteerDot}></span>
+
             <p>vibrant volunteer</p>
           </li>
-          <li>
-            
-            <p>fitness advocate</p>
+          <li
+            onClick={() => {
+              handleShowAllTraits("learner");
+            }}
+            onMouseOver={() => {
+              handleSelectionDotHover(true, "learner_dot");
+            }}
+            onMouseOut={() => {
+              handleSelectionDotHover(false, "learner_dot");
+            }}
+          >
+            <span style={learnerDot}></span>
+
+            <p>avid learner</p>
           </li>
-          <li>
+          <li style={fitnessVisibility}>
             <p> {fitness} </p>
+          </li>
+          <li style={resilientVisibility}>
+            <p> {resilient} </p>
+          </li>
+          <li style={volunteerVisibility}>
+            <p> {volunteer} </p>
+          </li>
+          <li style={learnerVisibility}>
+            <p> {learner} </p>
           </li>
         </div>
       </div>
       <div className={classes["grid-desc"]}></div>
 
-      <div className={classes["grid-traits"]}>
-        <div className={classes["trait"]}>
-          <li>
-            
-            <p>fitness advocate</p>
-          </li>
-          <li>
-            <p>buoyant beacon</p>
-          </li>
-          <li>
-            <p>vibrant volunteer</p>
-          </li>
-          <li>
-            <p>fitness advocate</p>
-          </li>
-        </div>
-        <div className={classes["container-trait-desc-border"]}>
-              <h2 className={classes["container-trait-desc"]}> {fitness} </h2>
-          </div>
-
-      </div>
     </>
   );
 }
--- components/career/career.jsx
+++ components/career/career.jsx
@@ -1,7 +1,73 @@
+"use client"
+
 import classes from "./career.module.css";
 import CareerCard from "./career-card";
+import { useState } from "react";
 
 export default function Career() {
+  const [showcard, setShowCard] = useState({
+    point1: false,
+    point2: false,
+    point3: false,
+
+  })
+
+  const handleShowCard = (point) => {
+    if (point === "point1") {
+      setShowCard(preValue => ({
+        ...preValue,
+        [point] : true
+      }))
+
+      if (showcard.point1) {
+        setShowCard(preValue => ({
+          ...preValue,
+          [point] : false
+        })) 
+      }
+    }
+    if (point === "point2") {
+      setShowCard(preValue => ({
+        ...preValue,
+        [point] : true
+      }))
+
+      if (showcard.point2) {
+        setShowCard(preValue => ({
+          ...preValue,
+          [point] : false
+        })) 
+      }
+    }
+
+    if (point === "point3") {
+      setShowCard(preValue => ({
+        ...preValue,
+        [point] : true
+      }))
+
+      if (showcard.point3) {
+        setShowCard(preValue => ({
+          ...preValue,
+          [point] : false
+        })) 
+      }
+    }
+
+
+  }
+
+  const showCardStyle1 = {
+    display : showcard.point1 ? "block" : "none"
+  }
+  const showCardStyle2 = {
+    display : showcard.point2 ? "block" : "none"
+  }
+  const showCardStyle3 = {
+    display : showcard.point3 ? "block" : "none"
+  }
+
+
   return (
     <>
       <div className={classes["page-title"]}>
@@ -10,17 +76,17 @@ export default function Career() {
       <div className={classes["grid-container"]}>
         <div className={classes["grid-container-left"]}>
           <div className={classes["vertical-line"]}>
-            <div className={classes["timeline-point-1"]}>
+            <div className={classes["timeline-point-1"]} >
               <div className={classes["pointdesc-education"]}>
                 <p className={classes["education-x"]}> History Graduate</p>
               </div>
             </div>
-            <div className={classes["timeline-point-2"]}>
+            <div className={classes["timeline-point-2"]} onClick={() => {handleShowCard("point1")}} >
               <div className={classes["pointdesc-career"]}>
-                <p className={classes["career-x"]}> Technology Teacher</p>{" "}
-              <div className={classes["card-1"]}> <CareerCard /> </div>
-                
-                
+                <p className={classes["career-x"]}> Technology Teacher</p>
+                <div className={classes["card-1"]} style={showCardStyle1}>
+                  <CareerCard />
+                </div>
               </div>
             </div>
             <div className={classes["timeline-point-3"]}>
@@ -31,35 +97,35 @@ export default function Career() {
             <div className={classes["timeline-point-4"]}>
               <div className={classes["pointdesc-education-2"]}>
                 <p className={classes["education-x-2"]}>
-                  {" "}
-                  Teachfirst Charity Ambassador{" "}
+                  Teachfirst Charity Ambassador
                 </p>
               </div>
             </div>
-            <div className={classes["timeline-point-5"]}>
+            <div className={classes["timeline-point-5"]} onClick={() => {handleShowCard("point2")}}>
               <div className={classes["pointdesc-career"]}>
-                <p className={classes["career-x-2"]}> Cloud Engineer</p>{" "}
-              <div className={classes["card-1"]}> <CareerCard /> </div>
-
+                <p className={classes["career-x-2"]}> Cloud Engineer</p>
+                <div className={classes["card-1"]} style={showCardStyle2}>
+                  <CareerCard />
+                </div>
               </div>
             </div>
             <div className={classes["timeline-point-6"]}>
               <div className={classes["pointdesc-education-3"]}>
                 <div className={classes["education-x-3"]}>
-                  {" "}
-                  <li> AWS Solution Architect </li>{" "}
+                  <li> AWS Solution Architect </li>
                   <li> Azure Administrator Associate</li>
-                </div>{" "}
+                </div>
               </div>
             </div>
-            <div className={classes["timeline-point-7"]}>
+            <div className={classes["timeline-point-7"]} onClick={() => {handleShowCard("point3")}}>
               <div className={classes["pointdesc-career"]}>
-                <p className={classes["career-x-2"]}> DevOps Engineer </p>{" "}
-              <div className={classes["card-1"]}> <CareerCard /> </div>
+                <p className={classes["career-x-2"]}> DevOps Engineer </p>
+                <div className={classes["card-1"]} style={showCardStyle3}>
+                  <CareerCard />
+                </div>
               </div>
             </div>
             <div className={classes["timeline-point-8"]}> </div>
-
           </div>
         </div>
       </div>
