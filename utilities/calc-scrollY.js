b8f1b4614b78c0f8f98b64ceeedfe19df1505f77
tomi-amao <tomi-amao@users.noreply.github.com>
2024-02-06 20:54:24 +0000

    Prettified Code!

--- utilities/calc-scrollY.js
+++ utilities/calc-scrollY.js
@@ -2,10 +2,10 @@ import { useMotionValueEvent, useScroll } from "framer-motion";
 
 export default function calcScrollY() {
   const { scrollY } = useScroll();
-  let scrollPosition = 0
+  let scrollPosition = 0;
 
   useMotionValueEvent(scrollY, "change", (latest) => {
-    scrollPosition = latest
-    });
-  return scrollPosition
+    scrollPosition = latest;
+  });
+  return scrollPosition;
 }
